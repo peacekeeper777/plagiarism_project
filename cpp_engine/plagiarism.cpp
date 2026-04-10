7e128d3af6c6d5a65e92d9a832fb11d8e63b6169
peacekeeper777 <amanchd2005@gmail.com>
2026-04-10 10:06:07 +0530

    new commit

--- cpp_engine/plagiarism.cpp
+++ cpp_engine/plagiarism.cpp
@@ -1,108 +1,83 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
-
-// Read file
-string readFile(string filename) {
+string readFile(const string& filename) {
     ifstream file(filename);
+    if (!file.is_open()) return "";
     stringstream buffer;
     buffer << file.rdbuf();
     return buffer.str();
 }
-
-// Convert to lowercase
-string preprocess(string text) {
+void preprocess(string& text) {
     transform(text.begin(), text.end(), text.begin(), ::tolower);
-    return text;
 }
-
-// Tokenize words
-vector<string> tokenize(string text) {
+vector<string> tokenize(const string& text) {
     vector<string> tokens;
-    string word = "";
-
-    for(char c : text) {
-        if(isspace(c)) {
-            if(word != "") tokens.push_back(word);
-            word = "";
-        } else {
-            word += c;
-        }
+    istringstream stream(text);
+    string word;
+    while (stream >> word) {
+        tokens.push_back(word);
     }
-    if(word != "") tokens.push_back(word);
     return tokens;
 }
-
-// Jaccard similarity
-double jaccard(vector<string> a, vector<string> b) {
-    set<string> s1(a.begin(), a.end());
-    set<string> s2(b.begin(), b.end());
-
+double jaccard(const vector<string>& a, const vector<string>& b) {
+    unordered_set<string> s1(a.begin(), a.end());
+    unordered_set<string> s2(b.begin(), b.end());
     int intersection = 0;
-    for(auto &w : s1) {
-        if(s2.count(w)) intersection++;
+    for (const auto& w : s1) {
+        if (s2.count(w)) intersection++;
     }
-
     int uni = s1.size() + s2.size() - intersection;
-    return (double)intersection / uni;
+    return (uni == 0) ? 0.0 : (double)intersection / uni;
 }
-
-// Generate n-grams
-vector<string> ngrams(vector<string> tokens, int n) {
+vector<string> ngrams(const vector<string>& tokens, int n) {
     vector<string> result;
-
-    for(int i = 0; i <= tokens.size() - n; i++) {
-        string gram = "";
-        for(int j = 0; j < n; j++) {
-            gram += tokens[i + j] + " ";
+    if (tokens.size() < n) return result;
+    for (size_t i = 0; i <= tokens.size() - n; i++) {
+        string gram = tokens[i];
+        for (int j = 1; j < n; j++) {
+            gram += " " + tokens[i + j];
         }
         result.push_back(gram);
     }
     return result;
 }
-
-// Find common patterns
-vector<string> commonPatterns(vector<string> a, vector<string> b) {
-    set<string> s1(a.begin(), a.end());
-    set<string> s2(b.begin(), b.end());
-
+vector<string> commonPatterns(const vector<string>& a, const vector<string>& b) {
+    unordered_set<string> s1(a.begin(), a.end());
+    unordered_set<string> s2(b.begin(), b.end());
     vector<string> common;
-
-    for(auto &w : s1) {
-        if(s2.count(w)) common.push_back(w);
+    for (const auto& w : s1) {
+        if (s2.count(w)) common.push_back(w);
     }
     return common;
 }
-
 int main(int argc, char* argv[]) {
-    if(argc < 3) {
+    if (argc < 3) {
         cout << "{\"error\":\"Provide two files\"}";
-        return 0;
+        return 1;
     }
-
-    string text1 = preprocess(readFile(argv[1]));
-    string text2 = preprocess(readFile(argv[2]));
-
+    string text1 = readFile(argv[1]);
+    string text2 = readFile(argv[2]);
+    preprocess(text1);
+    preprocess(text2);
     vector<string> t1 = tokenize(text1);
     vector<string> t2 = tokenize(text2);
-
-    double sim = jaccard(t1, t2) * 100;
-
+    double sim = jaccard(t1, t2) * 100.0;
     vector<string> n1 = ngrams(t1, 2);
     vector<string> n2 = ngrams(t2, 2);
-
     vector<string> patterns = commonPatterns(n1, n2);
-
-    // Output JSON
     cout << "{";
     cout << "\"similarity\":" << sim << ",";
     cout << "\"patterns\":[";
-
-    for(int i = 0; i < patterns.size(); i++) {
+    for (size_t i = 0; i < patterns.size(); i++) {
         cout << "\"" << patterns[i] << "\"";
-        if(i != patterns.size()-1) cout << ",";
+        if (i != patterns.size() - 1) cout << ",";
     }
-
     cout << "]}";
-
     return 0;
 }
